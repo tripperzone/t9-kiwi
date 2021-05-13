b9c5fb10a3e72115a4ae69ac32e4ba67d749bd39
Daniel <stormyer@hotmail.com>
2021-05-13 16:20:09 +0200

    feat(store): prepare reducer for suggestions

---
+++ client/src/store/reducers/suggestions.js
@@ -0,0 +1,70 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { createSelector } from "reselect";
+import { apiCallBegan } from "../api";
+
+const slice = createSlice({
+  name: "suggestions",
+  initialState: {
+    list: {
+        nodeLevel: [],
+        deepLevel: [],
+    },
+    loading: false
+  },
+  reducers: {
+    suggestionsRequested: (state) => {
+        state.loading = true;
+    },
+
+    suggestionsReceived: (state, action) => {
+        const { suggestions } = action.payload;
+        state.list = suggestions;
+        state.loading = false;
+    },
+
+    suggestionsRequestFailed: (state) => {
+        state.loading = false;
+    },
+ 
+    suggestionsCleared: (state) => {
+        state.list.nodeLevel = [];
+        state.list.deepLevel = [];
+    },
+  }
+});
+
+const {
+  suggestionsRequested,
+  suggestionsReceived,
+  suggestionsRequestFailed,
+  suggestionsCleared,
+} = slice.actions;
+
+export default slice.reducer;
+
+// Action Creators
+
+export const getSuggestions = (data) => dispatch => {
+    return dispatch(
+      apiCallBegan({
+        url: '/suggestions',
+        method: 'post',
+        data,
+        onStart: suggestionsRequested.type,
+        onSuccess: suggestionsReceived.type,
+        onError: suggestionsRequestFailed.type,
+      })
+    );
+};
+
+export const clearSuggestions = () => dispatch => {
+    return dispatch(
+        { type: suggestionsCleared.type }
+    )
+};
+
+// Selectors
+export const getSuggestionsList = createSelector(
+    state => state.suggestions,
+    suggestions => suggestions.list
+);
